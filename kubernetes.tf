4e050f8b3826e4aed267245494a2cf41e750ae8c
Mahidhar <43684464+mahidharu@users.noreply.github.com>
2021-07-31 00:06:09 -0500

    Updating

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,9 +1,5 @@
-data "azurerm_kubernetes_cluster" "cluster" {
-  name = azurerm_kubernetes_cluster.reactapp.name
-}
-
 provider "kubernetes" {
-  host = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
+  host = data.azurerm_kubernetes_cluster.reactapp.kube_config.0.host
 
   client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
   client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
