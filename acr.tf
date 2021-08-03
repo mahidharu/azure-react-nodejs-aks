16e5862ba77a3ffbed17ee61a3bd188314a0783f
Mahidhar <43684464+mahidharu@users.noreply.github.com>
2021-08-03 01:07:36 -0500

    Update

--- acr.tf
+++ acr.tf
@@ -15,49 +15,12 @@ data "azurerm_container_registry" "acr" {
   resource_group_name = "images"
 }
 
-/*resource "azurerm_role_assignment" "acrpull_role" {
-  scope                            = data.azurerm_container_registry.acr.id
-  role_definition_name             = "acrpull"
-  principal_id                     = data.azuread_service_principal.aks_sp.object_id
-  #skip_service_principal_aad_check = true
-}*/
-
-data "azurerm_subscription" "primary" {
-}
-
-data "azurerm_client_config" "current" {
-}
-
-output "account_id" {
-  value = data.azurerm_client_config.current.client_id
-}
-
-resource "azurerm_role_definition" "acrpull" {
-  role_definition_id = "00000000-0000-0000-0000-000000000000"
-  name               = "acrpull-role-definition"
-  scope              = data.azurerm_subscription.primary.id
-
-  permissions {
-    actions     = ["Microsoft.Resources/subscriptions/resourceGroups/read"]
-    not_actions = []
-  }
-
-  assignable_scopes = [
-    data.azurerm_subscription.primary.id
-  ]
-}
-
-resource "azurerm_role_assignment" "acrpull" {
-  name               = "00000000-0000-0000-0000-000000000000"
-  scope              = data.azurerm_subscription.primary.id
-  role_definition_id = azurerm_role_definition.acrpull.role_definition_resource_id
-  principal_id       = data.azurerm_client_config.current.object_id
+output "acr_id" {
+  value = data.azurerm_container_registry.acr.id
 }
 
 resource "azurerm_role_assignment" "acrpull_role" {
   scope                            = data.azurerm_container_registry.acr.id
-  role_definition_name             = "ACRPull"
-  #principal_id                     = data.azuread_service_principal.aks_sp.object_id
-  principal_id         = azurerm_kubernetes_cluster.reactapp.kubelet_identity[0].object_id
-  #skip_service_principal_aad_check = true
+  role_definition_name             = "AcrPull"
+  principal_id                    = azurerm_kubernetes_cluster.reactapp.kubelet_identity[0].object_id
 }
